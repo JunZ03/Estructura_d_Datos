94c1260eb0f4c335f95973b39b505d79c569c109
Juan Nicolás Zuluaga Jauregui <110936786+JunZ03@users.noreply.github.com>
2023-02-14 09:55:56 -0500

    Add files via upload

---
+++ Ciclo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cararteres.deescape;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author B11
+ */
+public class Ciclo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num = 4;
+        while (num <= 40) {
+            System.out.println(num);
+            num=num+4;
+        }
+
+    }
+}
